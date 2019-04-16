7d5d575ab602a93dd91d488005b34e9a6c12051b
ishan_modi <ishan_modi@Jiya>
2019-04-16 16:39:37 -0500

    Pushing the code for change request no#1234

---
+++ forgithub/src/github1/Githubclass.java
@@ -0,0 +1,17 @@
+package github1;
+
+public class Githubclass {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	Calculator basicCal = new Calculator();
+	basicCal.add(50, 10); 
+		}
+	public void day()
+	{
+		System.out.println("Today is sunday");
+	}
+	}
+
+
